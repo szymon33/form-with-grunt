3a4bd4fed62e552e54bcd1da8ea5ad844ff59609
szymonm <miedziejko@gmail.com>
2016-03-10 16:37:29 +0100

    Update distribution version in repository

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,14 @@
 (function() {
   $(document).foundation();
 
+  jQuery(function() {
+    return $(document).on("scroll", function() {
+      if ($(this).scrollTop() > 100) {
+        return $(".top-bar").removeClass("large").addClass("small");
+      } else {
+        return $(".top-bar").removeClass("small").addClass("large");
+      }
+    });
+  });
+
 }).call(this);
